93d77678163d4dc813e8f72628d5852da57fc79b
Caroline <caroline.reiss@sky.uk>
2019-08-09 11:17:24 +0100

    adding toggle button functionality

--- src/Components/SnackTile.js
+++ src/Components/SnackTile.js
@@ -1,21 +1,16 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import PBSandwich from '../assets/PBSandwich.jpeg'
+import classnames from 'classnames';
 
-class SnackTile extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { 
-            sancks: []
-         }
-    }
+function SnackTile() { 
 
-    
-    render() { 
+    const [cPanelIsOpen, closePanel] = useState(false);
+
         return (
             <div>
             <div className="o-layout o-layout--center">
   <div className="o-layout__item u-width-1/2@large">
-    <article className="c-tile c-tile--square is-selected">
+    <article className={classnames({'c-tile': true, 'c-tile--square': true, 'is-selected': cPanelIsOpen})}>
       <a className="c-tile__link c-shine-context" href="#">
         <div className="c-tile__content">
           <div className="c-tile__media">
@@ -23,6 +18,10 @@ class SnackTile extends Component {
           </div>
           <div className="c-tile__body c-tile__caption">
             <p className="c-tile__title c-heading-delta">Tile Title</p>
+            <button className="c-btn c-btn--primary" onClick={() => closePanel(!cPanelIsOpen)}>
+                <img className="c-btn__icon" src="https://www.sky.com/assets/toolkit/docs/buttons/example.svg" alt="Example Icon" />
+                { cPanelIsOpen ? "Show Less" : "Show More" }
+            </button>
           </div>
         </div>
         <div className="c-tile__shine c-tile__shine--top c-shine">
@@ -35,9 +34,8 @@ class SnackTile extends Component {
     </article>
   </div>
 </div>
-<article className="c-panel is-open">
+<article className={classnames({'c-panel': true, 'is-open': cPanelIsOpen})}>
   <div className="c-panel__content">
-    <a className="c-panel__toggle" href="#1">Close</a>
     <div className="o-container">
       <p className="c-text-body  u-text-constrain">
        This is a peanut butter sandwich!!!x
@@ -48,6 +46,5 @@ class SnackTile extends Component {
 </div>
           );
     }
-}
  
 export default SnackTile;
