c2713c552c1261bbe38d068daed9c88bc6004def
Encrypted Account <gradacad@ostml0245152.bskyb.com>
2019-08-09 15:38:12 +0100

    Take out debugging code

--- src/App.js
+++ src/App.js
@@ -37,7 +37,6 @@ function App(props) {
         );
       }
     }
-    console.log(results);
     setResults(results);
   };
 
--- src/Components/FeelingLucky.jsx
+++ src/Components/FeelingLucky.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { tsPropertySignature } from '@babel/types';
 
 function FeelingLucky(props) {
     return (
