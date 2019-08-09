2df1b4972d4413ed053355c7fa886e489abea355
Jack <jackbarber1998@hotmail.com>
2019-08-09 13:20:22 +0100

    Adds footer + disclaimer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import SnackList from './Components/SnackList';
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <div className="App">
       <SnackList />
+      <Footer />
     </div>
   );
 }
