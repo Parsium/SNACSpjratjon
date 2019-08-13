56fd3077aabbb1e7e440192ce321ff3bcd568e3e
Sarah Markless <sarah.markless2@sky.uk>
2019-08-13 11:03:58 +0100

    so accessible now wowww

--- src/Components/SnackList.js
+++ src/Components/SnackList.js
@@ -3,41 +3,23 @@ import SnackTile from "./SnackTile.js";
 import "../Styles/SnackList.css";
 
 export const SnackList = props => {
-<<<<<<< HEAD
-    return (
-      <div className="snack-list">
-        <h1 id="snackList-header" className= "c-heading-alpha">Your recommended snacks!</h1>
-        <div className="snack-list-container">
-          {props.snacks.map(snack => (
-            <SnackTile
-              key={snack.id}
-              snackName={snack.name}
-              image={snack.image}
-              snackDescription={snack.description}
-              calories={snack.nutrition.cal}
-              sugar={snack.nutrition.sugar}
-            />
-          ))}
-        </div>
-      </div>
-    );
-=======
   return (
     <div className="snack-list">
-      <h2>Your recommended snacks!</h2>
+      <h1 id="snackList-header" className="c-heading-alpha">
+        Your recommended snacks!
+      </h1>
       <div className="snack-list-container">
         {props.snacks.map(snack => (
           <SnackTile
             key={snack.id}
             snackName={snack.name}
+            image={snack.image}
             snackDescription={snack.description}
             calories={snack.nutrition.cal}
             sugar={snack.nutrition.sugar}
-            image={snack.image}
           />
         ))}
       </div>
     </div>
   );
->>>>>>> origin/master
 };
