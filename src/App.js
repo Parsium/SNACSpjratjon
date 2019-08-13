c59beb99371ef2b521d93593fd05272b23b5caa0
Sarah Markless <sarah.markless2@sky.uk>
2019-08-13 09:56:23 +0100

    resolving conflict

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React, { useState } from "react";
-<<<<<<< HEAD
-import {SnackList} from "./Components/SnackList";
-=======
->>>>>>> origin/master
 import { Header } from "./Components/Header";
 import { SnackList } from "./Components/SnackList";
 import Footer from "./Components/Footer";
