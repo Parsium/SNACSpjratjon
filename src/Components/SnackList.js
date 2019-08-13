dd27353fccc02eb87ef2ded0cdce7ea3a7678527
Caroline <caroline.reiss@sky.uk>
2019-08-13 12:55:04 +0100

    stylings assets

--- src/Components/SnackList.js
+++ src/Components/SnackList.js
@@ -3,7 +3,6 @@ import SnackTile from "./SnackTile.js";
 import "../Styles/SnackList.css";
 
 export const SnackList = props => {
-<<<<<<< HEAD
     return (
       <div className="snack-list">
         <h1 id="snackList-header" className= "c-heading-alpha">Your recommended snacks!</h1>
@@ -21,23 +20,4 @@ export const SnackList = props => {
         </div>
       </div>
     );
-=======
-  return (
-    <div className="snack-list">
-      <h2>Your recommended snacks!</h2>
-      <div className="snack-list-container">
-        {props.snacks.map(snack => (
-          <SnackTile
-            key={snack.id}
-            snackName={snack.name}
-            snackDescription={snack.description}
-            calories={snack.nutrition.cal}
-            sugar={snack.nutrition.sugar}
-            image={snack.image}
-          />
-        ))}
-      </div>
-    </div>
-  );
->>>>>>> origin/master
 };
