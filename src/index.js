18e406a17c3dfe27318d7ecf3c4447da05e6d2ff
Jack <jackbarber1998@hotmail.com>
2019-08-20 16:51:36 +0100

    breaks some stuff to fix

--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import data from "./snacks.json";
 import * as serviceWorker from "./serviceWorker";
+import { InMemoryCache } from "apollo-boost";
+import { HttpLink } from "apollo-link-http";
+import { ApolloClient } from "apollo-client";
+import { gql } from "apollo-boost";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+    uri: "http://localhost:4000/"
+});
+
+const client = new ApolloClient({
+    cache,
+    link,
+});
+
+client.query({
+    query: gql`
+    query {
+        snack {
+          id
+          name
+          description
+          nutrition {
+            sugar
+            calories
+          }
+          dietary {
+            vegetarian
+            vegan
+            nutFree
+            glutenFree
+          }
+        }
+      }
+    `
+  }).then(result => console.log(result));
 
 ReactDOM.render(<App data={data} />, document.getElementById("root"));
 
